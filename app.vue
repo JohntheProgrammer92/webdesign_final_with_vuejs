5be1616cb92a245154dfb81d549c03dd45a20012
johntheprogrammer92 <johnboner92@gmail.com>
2019-09-16 02:13:34 -0400

    Added VUE.js and made the site into a SPA.

---
+++ app.vue
@@ -0,0 +1,29 @@
+
+
+            var app = new Vue({
+              el: '#app',
+              data: {
+                counter: 0, 
+                home: true,
+                second: false,
+                third: false,
+                fourth: false,
+                ref: false
+              },
+              methods: {
+                selecthome: function(){
+                  this.home === true;
+                  this.second === false;
+                  this.third === false;
+                  this.fourth === false;
+                  this.ref === false;
+                }
+              },
+              select2: function(){
+                  this.home = false
+                  this.second = true
+                  this.third = false
+                  this.fourth = false
+                  this.ref = false
+                }
+            })
